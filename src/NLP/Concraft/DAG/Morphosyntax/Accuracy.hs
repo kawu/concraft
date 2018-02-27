9a5b88254f3fc64ba84b40abe11cc578fee199a7
Jakub Waszczuk <waszczuk.kuba@gmail.com>
2018-02-27 21:08:22 +0100

    Move functions responsible for recognizing segmentation-level ambiguities to a separate module

--- src/NLP/Concraft/DAG/Morphosyntax/Accuracy.hs
+++ src/NLP/Concraft/DAG/Morphosyntax/Accuracy.hs
@@ -21,7 +21,6 @@ import           Prelude hiding (Word)
 import           GHC.Conc (numCapabilities)
 
 import qualified Control.Parallel.Strategies as Par
-import qualified Data.MemoCombinators as Memo
 
 import           Data.List (transpose)
 import qualified Data.Foldable as F
@@ -31,6 +30,8 @@ import qualified Data.Tagset.Positional as P
 
 import qualified Data.DAG as DAG
 import           NLP.Concraft.DAG.Morphosyntax
+import           NLP.Concraft.DAG.Morphosyntax.Ambiguous
+  (identifyAmbiguousSegments)
 -- import           NLP.Concraft.DAG.Morphosyntax.Align
 
 import qualified Data.Text as T
@@ -172,55 +173,6 @@ recall :: Stats -> Double
 recall Stats{..}
   = fromIntegral tp
   / fromIntegral (tp + fn)
-
-
-------------------------------------------------------
--- Marking segmantation ambiguities
-------------------------------------------------------
-
-
--- | Identify ambigouos segments (roughly, segments which can be by-passed) in
--- the given DAG. Such ambiguous edges are marked in the resulting DAG with
--- `True` values.
-identifyAmbiguousSegments :: DAG.DAG a b -> DAG.DAG a Bool
-identifyAmbiguousSegments dag =
-  flip DAG.mapE dag $ \edgeID _ ->
-    incoming edgeID * outgoing edgeID < totalPathNum
-  where
-    incoming = inComingNum dag
-    outgoing = outGoingNum dag
-    totalPathNum = sum
-      [ outgoing edgeID
-      | edgeID <- DAG.dagEdges dag
-      , DAG.isInitialEdge edgeID dag ]
-
-
--- | Compute the number of paths from a starting edge to the given edge.
-inComingNum :: DAG.DAG a b -> DAG.EdgeID -> Int
-inComingNum dag =
-  incoming
-  where
-    incoming =
-      Memo.wrap DAG.EdgeID DAG.unEdgeID Memo.integral incoming'
-    incoming' edgeID
-      | DAG.isInitialEdge edgeID dag = 1
-      | otherwise = sum $ do
-          prevID <- DAG.prevEdges edgeID dag
-          return $ incoming prevID
-
-
--- | Compute the number of paths from the given edge to a target edge.
-outGoingNum :: DAG.DAG a b -> DAG.EdgeID -> Int
-outGoingNum dag =
-  outgoing
-  where
-    outgoing =
-      Memo.wrap DAG.EdgeID DAG.unEdgeID Memo.integral outgoing'
-    outgoing' edgeID
-      | DAG.isFinalEdge edgeID dag = 1
-      | otherwise = sum $ do
-          nextID <- DAG.nextEdges edgeID dag
-          return $ outgoing nextID
 
 
 ------------------------------------------------------
---
+++ src/NLP/Concraft/DAG/Morphosyntax/Ambiguous.hs
@@ -0,0 +1,64 @@
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE OverloadedStrings #-}
+
+
+-- | Segmentation-level ambiguities.
+
+
+module NLP.Concraft.DAG.Morphosyntax.Ambiguous
+  ( identifyAmbiguousSegments
+  ) where
+
+
+import qualified Data.MemoCombinators as Memo
+import qualified Data.DAG as DAG
+
+
+------------------------------------------------------
+-- Marking segmantation ambiguities
+------------------------------------------------------
+
+
+-- | Identify ambigouos segments (roughly, segments which can be by-passed) in
+-- the given DAG. Such ambiguous edges are marked in the resulting DAG with
+-- `True` values.
+identifyAmbiguousSegments :: DAG.DAG a b -> DAG.DAG a Bool
+identifyAmbiguousSegments dag =
+  flip DAG.mapE dag $ \edgeID _ ->
+    incoming edgeID * outgoing edgeID < totalPathNum
+  where
+    incoming = inComingNum dag
+    outgoing = outGoingNum dag
+    totalPathNum = sum
+      [ outgoing edgeID
+      | edgeID <- DAG.dagEdges dag
+      , DAG.isInitialEdge edgeID dag ]
+
+
+-- | Compute the number of paths from a starting edge to the given edge.
+inComingNum :: DAG.DAG a b -> DAG.EdgeID -> Int
+inComingNum dag =
+  incoming
+  where
+    incoming =
+      Memo.wrap DAG.EdgeID DAG.unEdgeID Memo.integral incoming'
+    incoming' edgeID
+      | DAG.isInitialEdge edgeID dag = 1
+      | otherwise = sum $ do
+          prevID <- DAG.prevEdges edgeID dag
+          return $ incoming prevID
+
+
+-- | Compute the number of paths from the given edge to a target edge.
+outGoingNum :: DAG.DAG a b -> DAG.EdgeID -> Int
+outGoingNum dag =
+  outgoing
+  where
+    outgoing =
+      Memo.wrap DAG.EdgeID DAG.unEdgeID Memo.integral outgoing'
+    outgoing' edgeID
+      | DAG.isFinalEdge edgeID dag = 1
+      | otherwise = sum $ do
+          nextID <- DAG.nextEdges edgeID dag
+          return $ outgoing nextID
